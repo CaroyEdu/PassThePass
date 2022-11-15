af119857fd05cfb31f04bdd07d66081af414c480
RoyEdu <eduardoroy@uma.es>
2022-11-15 19:31:15 +0100

    Add Password Working

--- PassThePass/app/src/main/java/com/example/passthepass/PTPHome.java
+++ PassThePass/app/src/main/java/com/example/passthepass/PTPHome.java
@@ -1,23 +1,34 @@
 package com.example.passthepass;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.passthepass.ui.my_account.MyAccountFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.passthepass.databinding.ActivityPtphomeBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PTPHome extends AppCompatActivity {
 
     private ActivityPtphomeBinding binding;
+    private FloatingActionButton floatingActionButton;
+    private EditText editTextAppName, editTextAppPassword;
+    private Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +37,9 @@ public class PTPHome extends AppCompatActivity {
         binding = ActivityPtphomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Bundle bundle = getIntent().getExtras();
+        bundle = getIntent().getExtras();
+
+        floatingActionButton = findViewById(R.id.floatingActionButton);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -37,6 +50,35 @@ public class PTPHome extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_ptphome);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public void createPassword(View view){
+        SQLiteDatabase db = new DBHelper(this).getWritableDatabase();
+
+        editTextAppName = findViewById(R.id.editTextAppName);
+        editTextAppPassword = findViewById(R.id.editTextAppPassword);
+
+        ContentValues values = new ContentValues();
+        values.put(DBContract.PasswordEntry.COLUMN_PASSWORD_APP, editTextAppName.getText().toString());
+        values.put(DBContract.PasswordEntry.COLUMN_PASSWORD_PASSWORD, editTextAppPassword.getText().toString());
+        values.put(DBContract.PasswordEntry.COLUMN_SHARED, "0");
+        long newRowId = db.insert(DBContract.PasswordEntry.TABLE_PASSWORD, null, values);
+
+        ContentValues values2 = new ContentValues();
+        values2.put(DBContract.UserPasswordEntry.COLUMN_USER_ID, bundle.get("id").toString());
+        values2.put(DBContract.UserPasswordEntry.COLUMN_PASSWORD_ID, String.valueOf(newRowId));
+        db.insert(DBContract.UserPasswordEntry.TABLE_USERPASSWORD, null, values2);
+    }
+
+    public void onClickCreate(View view){
+        replaceFragment(new CreatePasswordFragment());
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.nav_host_fragment_activity_ptphome, fragment);
+        fragmentTransaction.commit();
     }
 
 }
--- PassThePass/app/src/main/java/com/example/passthepass/ui/my_passwords/MyPasswordsFragment.java
+++ PassThePass/app/src/main/java/com/example/passthepass/ui/my_passwords/MyPasswordsFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
